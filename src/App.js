b37ef7adc4db1f086f3716d214a58f42f28ca802
vanduc19it <duc19it@gmail.com>
2022-09-04 16:21:10 +0700

    todo react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './components/TodoList'
+import Textfield from '@atlaskit/textfield'
+import Button from '@atlaskit/button'
+import {useState, useCallback, useEffect} from 'react'
+import { v4 } from 'uuid'
+
+const STORAGE_KEY = 'TODO_APP';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([])
+  const [textInput, setTextInput] = useState("")
+
+  
+
+  useEffect(() => {
+    const storagedTodoList = localStorage.getItem(STORAGE_KEY);
+    if(storagedTodoList){
+      setTodoList(JSON.parse(storagedTodoList));
+    }
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todoList))
+  },[todoList])
+
+  const onTextInputChange = useCallback((e) => {
+    setTextInput(e.target.value)
+  },[])
+
+
+  const onAddBtnClick = useCallback((e) => {
+    setTodoList([{id: v4(), name: textInput, isCompleted: false},...todoList])
+
+    setTextInput("")
+
+  },[textInput, todoList])
+
+
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList((prevState) => prevState.map((todo) => todo.id === id ? {...todo, isCompleted: true} : todo))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h3>Danh Sách Cần Làm</h3>
+      <Textfield name ="add-todo" placeholder ="Thêm việc cần làm..." elemAfterInput=
+        {<Button isDisabled={!textInput} appearance='primary' onClick={onAddBtnClick}>Thêm</Button>
+        }
+        css ={{padding: '2px 4px 2px'} }
+        value = {textInput}
+        onChange={onTextInputChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Textfield>
+     
+      <TodoList todoList= {todoList} onCheckBtnClick= {onCheckBtnClick} />
+    </>
+    
   );
 }
 
